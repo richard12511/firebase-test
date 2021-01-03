a0e247ef7874fc29dfcd4af6aa565fbfece3b0ce
Richard Schmidt <richardnew12511@gmail.com>
2021-01-03 13:22:48 -0600

    first commit

---
+++ firebase-test/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase-test
+//
+//  Created by Richard Schmidt on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
